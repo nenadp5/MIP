a329aaeb2803fa524814ac3a955dcaf41f83b511
Nenad <nenadpecic05@gmail.com>
2026-03-20 18:09:47 +0100

    Update 11.asm

--- Računske vežbe/11.asm
+++ Računske vežbe/11.asm
@@ -3,6 +3,7 @@
 
 ;Prvo moramo da nadjemo TH1 preko fomrule i da proverimo kako ce njegovo zaokruzivanje uticati na Baud Rate, gde odstupanje ne sme biti vece od 2%
 ;Formula: Baud Rate=(2^smod / 32) * (Fkristala / (12*(256-TH1)) | Baud Rate=dato u tekstu | smod=0 | Fkristala=dato u tekstu | TH1=trazimo i zaokruzujemo na prvi priblizni broj
+;Ukoliko odstupanje Baud Rate-a bude vece od 2% onda se ne pise program za inicijalizaciju.
 
 ;UART - 8 bitni rezim rada i podesavamo u SCON registru. Sadrzaj SCON registra je 01010000 odnosno #50h
 ;Tajmer T1 - sa 8 bitnim autoreload funkcijom i podesavamo u TMOD registru. Sadrzaj TMOD registra je 00100000 odnosno #20h
